6625cffffdbfb3ac92f90ab52f5effd843e4c8fc
Ivan <ivanjca93@gmail.com>
2020-01-14 11:24:36 -0300

    Login normal, sección 10

--- server/server.js
+++ server/server.js
@@ -1,49 +1,27 @@
-const express = require('express')
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
-require('./config/config');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(require('../routes/index'));
+
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (err) throw err;
 
-app.post('/usuario/', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-
-        res.json({
-            body
-        });
-
-    }
-
-
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+    console.log('Base de datos Online');
 });
 
 app.listen(process.env.PORT, () => {
